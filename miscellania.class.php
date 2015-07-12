52b7dc2b398ae27da09cab48371ca84519c6867b
joseraimundo165 <joseraimundo165@yahoo.com.br>
2015-07-12 18:49:51 -0300

    Numero aleatorio
    Random

--- miscellania.class.php
+++ miscellania.class.php
@@ -1,5 +1,13 @@
 <?php
-class Miscellania {
+class Miscellania { 
+$numero = 12;
+$min = 1;
+$limite = 9999;
+$gera = rand($min,$limite);
+ if($gera == $numero) 
+echo ''Você acertou com o numero: ".$numero;
+else
+echo " Você errou, tente denovo.";
 
 }
 ?>
